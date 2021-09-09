32628d8d8dd9b54e4a349e176431c5e028ec865d
José Bezerra <josbezfi@gmail.com>
2021-09-09 19:58:01 -0300

    Usando o PreparedStatement para evitar o uso de SQL Injection.

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -1,23 +1,24 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
-        //Informações que simulam a entrada de dados pelo cliente
+
         String nome = "Mouse'";
         String descricao = "Logitech MX); delete from produto;"; //Perceba o SQL Injection
-        // O prejuízo pode ser a exclusão da tabela
 
-        //Inserindo elementos no banco
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
 
-        Statement stm = connection.createStatement();
-        stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('" + nome + " ', '"+ descricao + "')",
+        //PreparedStatement evita o uso intencional de SQL Injection
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
+        stm.setString(1, nome);
+        stm.setString(2, descricao);
+
+        stm.execute();
+
         ResultSet rst = stm.getGeneratedKeys();
+
         while (rst.next()) {
             int id = rst.getInt(1);
             System.out.printf("O id gerado foi %d", id);
