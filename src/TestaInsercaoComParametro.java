b35273af845406b208b3379c6c5598248c477e2c
José Bezerra <josbezfi@gmail.com>
2021-09-10 22:38:21 -0300

    Simulando falha na transação do banco. O primeiro produto é adicionado, segundo lança Exception.

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -3,17 +3,23 @@ import java.sql.*;
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
 
-        String nome = "Mouse'";
-        String descricao = "Logitech MX); delete from produto;"; //Perceba o SQL Injection
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
 
         //PreparedStatement evita o uso intencional de SQL Injection
         PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
+        adicionarVariavel("Smart TV", "LG", stm);
+        adicionarVariavel("Ipad", "Apple", stm);
+    }
+
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
+
+        if (nome.equals("Ipad")) {
+            throw new RuntimeException("Não foi possível adicionar o produto!");
+        }
 
         stm.execute();
 
@@ -21,7 +27,7 @@ public class TestaInsercaoComParametro {
 
         while (rst.next()) {
             int id = rst.getInt(1);
-            System.out.printf("O id gerado foi %d", id);
+            System.out.printf("O id gerado foi %d%n", id);
         }
     }
 }
