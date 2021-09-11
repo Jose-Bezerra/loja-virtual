a92acaeb87ccba3e45bd3e13ba4a9d713e412c9f
José Bezerra <josbezfi@gmail.com>
2021-09-11 12:31:42 -0300

    Criação do ProdutoDao para isolar o código que manipula objetos no banco de dados.

--- src/TestaInsercaoComProduto.java
+++ src/TestaInsercaoComProduto.java
@@ -1,26 +1,15 @@
+import dao.ProdutoDAO;
 import modelo.Produto;
 
 import java.sql.*;
 
-public class TestaInsercaoComProduto {
+    public class TestaInsercaoComProduto {
     public static void main(String[] args) throws SQLException {
         Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 
         try (Connection connection = new ConnectionFactory().recuperarConexao()){
-            String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
-
-            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-                pstm.setString(1, comoda.getNome());
-                pstm.setString(2, comoda.getDescricao());
-
-                pstm.execute();
-
-                try (ResultSet rst = pstm.getGeneratedKeys()){
-                    while (rst.next()) {
-                        comoda.setId(rst.getInt(1));
-                    }
-                }
-            }
+            ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+            produtoDAO.salvar(comoda);
         }
 
         System.out.println(comoda);
